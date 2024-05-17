1bea1f5f4b882edbe0084695ed41f963a7f4568c
Tim Otten <totten@civicrm.org>
2024-05-17 00:30:55 -0700

    examples/

---
+++ examples/setting-definition.php
@@ -0,0 +1,19 @@
+<?php
+use Civi\Core\SettingsDefinition;
+use CRM_Mosaico_ExtensionUtil as E;
+
+// About this doc
+return SettingsDefinition::create([
+  /* The name is important */
+  'name' => 'hello',
+  // The label is shown to somebody
+  'label' => E::ts('Hello World!'),
+  'active' => TRUE,
+  'html' => [
+    // To boldly go
+    'bold' => TRUE,
+  ],
+  'details' => fn() => [
+    'alskjdf asdf' => 123,
+  ]
+]);
---
+++ examples/simple-array.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+  'name' => 'hello',
+  'label' => 'Hello World!',
+  'active' => TRUE,
+  'html' => [
+    'bold' => TRUE,
+  ],
+  'details' => fn() => [
+    'alskjdf asdf' => 123,
+  ]
+];
